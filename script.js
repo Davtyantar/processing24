bb91d2059d5a428a657a3806753deb78bea179b8
Davtyantar <107843395+Davtyantar@users.noreply.github.com>
2022-11-07 22:53:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// const burger = document.querySelector('.burger');
+// const menuItem1 = document.querySelector('.menu_item_media1');
+// const menuItem2 = document.querySelector('.menu_item_media2');
+// const menuItem3 = document.querySelector('.menu_item_media3');
+// const menuItem4 = document.querySelector('.menu_item_media4');
+// const menuItem5 = document.querySelector('.menu_item_media5');
+// const menuItem6 = document.querySelector('.menu_item_media6');
+
+
+
+// // burger.onclick = function() {
+// //     menuItems.classList.toggle = "asd"
+// //     console.log('hi');
+// // }
+
+// burger.onclick = function() {
+//     let hidden = true;
+//     console.log('Кнопка нажата!');
+//     menuItem1.classList.remove('menu_item_media1');
+//     menuItem1.classList.add('menu_item_media_toggle1');
+//     menuItem2.classList.remove('menu_item_media2');
+//     menuItem2.classList.add('menu_item_media_toggle2');
+//     menuItem3.classList.remove('menu_item_media3');
+//     menuItem3.classList.add('menu_item_media_toggle3');
+//     menuItem4.classList.remove('menu_item_media4');
+//     menuItem4.classList.add('menu_item_media_toggle4');
+//     menuItem5.classList.remove('menu_item_media5');
+//     menuItem5.classList.add('menu_item_media_toggle5');
+//     menuItem6.classList.remove('menu_item_media6');
+//     menuItem6.classList.add('menu_item_media_toggle6');
+// };
+
